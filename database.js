c1d9b5e2d103a58bbaabbb47745aa311ed0c43fe
SageSeven <49072230+SageSeven@users.noreply.github.com>
2019-11-26 19:21:44 +0800

    Update database.js
    Try to handle disconnection

--- database.js
+++ database.js
@@ -10,7 +10,49 @@ const database = mysql.createConnection({
 	database: config.database
 });
 
-database.connect();
+database.connect((err)=>{
+	if (err) {
+		setTimeout(handleDisconnection, 2000);
+	}
+});
+
+database.on('error', ()=>{
+	logger.error('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+        logger.error('db error执行重连:'+err.message);
+        handleDisconnection();
+    } else {
+        throw err;
+    }
+});
+
+function handleDisconnection() {
+	let connection = mysql.createConnection({
+		host: config.host,
+		user: config.user,
+		port: config.port,
+		password: config.password,
+		database: config.database
+	});
+
+	connection.connect((err)=>{
+		if (err) {
+			setTimeout(handleDisconnection, 2000);
+		}
+	});
+
+	connection.on('error', ()=>{
+		logger.error('db error', err);
+	    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+	        logger.error('db error执行重连:'+err.message);
+	        handleDisconnection();
+	    } else {
+	        throw err;
+	    }
+	});
+
+	module.exports = connection;
+}
 
 console.log("mysql connected.")
 
